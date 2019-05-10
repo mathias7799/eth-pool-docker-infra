de6779dd356365ffd34a0c2666baab6c318653c5
mathias7799 <mathias77992@gmail.com>
2019-05-10 17:40:35 +0200

    Update run-redis-cmd.sh

--- redis-cmd/run-redis-cmd.sh
+++ redis-cmd/run-redis-cmd.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Starting redis-cmd..."
-screen -dmS redis-cmd redis-commander --redis-port 6379 --redis-host 127.0.0.1 --port 8081 --address 0.0.0.0
+screen -dmS redis-cmd redis-commander --redis-port 6379 --redis-host 127.0.0.1 --port 8081 --http-u mathias7799 --http-p Editingthisonuse
 echo "Done."
 
 exit 0
