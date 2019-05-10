4dba14f02181c0e55104071165401a319fb78ee7
mathias7799 <mathias77992@gmail.com>
2019-05-10 17:33:53 +0200

    Update run-geth.sh

--- geth/run-geth.sh
+++ geth/run-geth.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Starting geth-01..."
-screen -dmS geth-01 /usr/bin/geth --datadir ./data --port 42370 --rpc --rpcport 13270 --cache=1024
+screen -dmS goegem ./home/go-egem/build/bin/egem --datadir /home/livenet/ --rpcaddr "127.0.0.1" --rpcport "8545" --maxpeers 100 --unlock 0x62896f0748730a121c3e42c9a1fdc58b9212a9c4 --rpc --rpccorsdomain "*" --extradata “mathias” --etherbase 0x62896f0748730a121c3e42c9a1fdc58b9212a9c4  --password="/home/mathiaspool/.1pass" --allow-insecure-unlock --mine
 echo "Done."
 
 exit 0
